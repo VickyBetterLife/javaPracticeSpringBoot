74850dea8abf61f84bb08f70d34d7e52e60a1dd1
yangh <vicky.yang@igt.com>
2019-09-27 10:21:31 +0800

    test

--- src/test/java/AmazonQuestions/treasureIslandtest.java
+++ src/test/java/AmazonQuestions/treasureIslandtest.java
@@ -1,6 +1,18 @@
 package AmazonQuestions;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class treasureIslandtest {
+
+    @Test
+    public void findTreasurePoint(){
+        TreasureIsland ti = new TreasureIsland();
+        char[][] input = new char[][]{{'O', 'O', 'O', 'O'},{'D', 'O', 'D', 'O'},{'O', 'O', 'O', 'O'},{'X', 'D', 'D', 'O'}};
+
+        int result = ti.minRouts_method2(input);
+        Assert.assertEquals(5,result);
+    }
 
 
 }
